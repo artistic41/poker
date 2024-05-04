e1d3a7dea4c7a55e497f152f250ba7f1ed7a55fc
Artistic Software <artisticsoftware7@gmail.com>
2024-05-04 03:48:02 +0000

    ...

--- classes/Hand.php
+++ classes/Hand.php
@@ -15,7 +15,21 @@ class Hand{
     function showHand(){
         return [$this->card1->showCard(), $this->card2->showCard()];
     }
+    function getHighCard($communityCards){
+        $allCards = [$this->card1, $this->card2];
+        $allCards = array_merge($allCards, $communityCards);
+        $allCardValues = [];
+        foreach($allCards as $card){
+            $value = $card->getValue();
+            $pos = array_search($value, Deck::$VALUES);
+            if(isset($allCardValues[$pos])) $allCardValues[$pos][] = $value;
+            else $allCardValues[$pos] = [$value];
+        }
+        $maxKey = max(array_keys($allCardValues));
+        return Deck::$VALUES[$maxKey];
+    }
     function evaluate($communityCards){
-        var_dump($communityCards); die();
+        $highCard = $this->getHighCard($communityCards);
+        if($this->isStraightFlush()) return "Straight Flush - High $highCard";
     }
 }
